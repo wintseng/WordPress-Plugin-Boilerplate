087f710061b03a747ca383bbe4f29f4356b2281e
wintseng <dustchen@gmail.com>
2024-03-17 13:49:51 +0800

    fix header message error

--- plugin-name.php
+++ plugin-name.php
@@ -1,28 +1,16 @@
 <?php
 
 /**
- * The plugin bootstrap file
+ * Plugin Name:     Plugin_Name
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     This is a plugin
+ * Author:          author
+ * Author URI:      http://example.com
+ * Text Domain:     plugin-name
+ * Domain Path:     /languages
+ * Version:         0.1.0
  *
- * This file is read by WordPress to generate the plugin information in the plugin
- * admin area. This file also includes all of the dependencies used by the plugin,
- * registers the activation and deactivation functions, and defines a function
- * that starts the plugin.
- *
- * @link              http://example.com
- * @since             1.0.0
- * @package           Plugin_Name
- *
- * @wordpress-plugin
- * Plugin Name:       WordPress Plugin Boilerplate
- * Plugin URI:        http://example.com/plugin-name-uri/
- * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
- * Version:           1.0.0
- * Author:            Your Name or Your Company
- * Author URI:        http://example.com/
- * License:           GPL-2.0+
- * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
- * Text Domain:       plugin-name
- * Domain Path:       /languages
+ * @package         Plugin_Name
  */
 
 // If this file is called directly, abort.
